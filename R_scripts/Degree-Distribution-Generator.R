605ae9f0cc5bfd931d6a42cf2d020b22a558e012
Brandon Klein <brandonklein77@googlemail.com>
2016-09-27 17:28:49 -0700

    changed chart parameters and included .jpeg output

--- R_scripts/Degree-Distribution-Generator.R
+++ R_scripts/Degree-Distribution-Generator.R
@@ -1,19 +1,16 @@
 # Script written by Brandon J. Klein
 # Degree-Distribution-Generator
-# Last Updated: 2016-09-20
+# Last Updated: 2016-09-27
 
-# For next week (9/28): Add introduction to function. Consider stringing together w/ random network generator.
-# Explore further edits to the aesthetic of the bar graph (e.g. add lines & x-axis).
-# Output bar graph to working directory. Specify file name and type.
-# Add more extensive comments. See if the code can be easily broken.
+# For next week (9/28): Add introduction to function and more extensive comments
+# See if the code can be easily broken.
+# Address nomenclature issues (e.g. genes.controlling).
+# Address setting wd.
 
-# Set working directory to Desktop (uncomment to use)
-# setwd("~/Desktop")
-
-degree.distribution = function(random.network) {
+degree.distribution = function(random.network,chart.title) {
 	
-	# Save column sums.
-	cSums=colSums(random.network)
+	# Set working directory (change as desired)
+	setwd("~/Desktop")
 	
 	# Save row sums.
 	rSums=rowSums(random.network)
@@ -44,11 +41,55 @@ degree.distribution = function(random.network) {
 
 	# Save the transpose of the degree distribution table.
 	Degree.Distribution.Transpose=t(Labeled.Degree.Distribution.Table)
+	
+	# Calculate maximum value to be displayed on y-axis
+	all.y=c(Degree.Distribution.Table$"In-Degree",Degree.Distribution.Table$"Out-Degree")
+	max.y=max(all.y)
+	
+	# Find out maximum number of genes in network
+	genes.affected=length(rowSums(random.network))
+	genes.controlling=length(colSums(random.network))	
+	max.genes=ifelse(genes.affected>genes.controlling,genes.affected,genes.controlling)
+	
+	# Calculate number of edges in input network
+	edges=sum(rowSums(random.network))
+	
+	# Create a name for the .jpeg file
+	filename=paste(max.genes,"-genes_",edges,"-edges_","degree-distribution",format(Sys.time(),"_%Y%m%d_%H.%M.%S"),".jpg",sep="")
 
+	# Save subsequent plotting to a .jpeg file
+	jpeg(filename)
+	
 	# Plot the degree distribution table.
-	barplot(Degree.Distribution.Transpose,
+	barplot=barplot(Degree.Distribution.Transpose,
 			beside=TRUE,
-			legend=TRUE,
+			col=c("black","gray"),
+			axes=FALSE,
 			xlab="Degree of Gene", font.lab=2,
-			ylab="Number of Genes with Given Degree")
+			ylab="Number of Genes with Given Degree",
+			yaxt="n",
+			main=chart.title)
+	
+	# Draw y-axis
+	axis(2,at=c(0:max.y),las=1,pos=0.3)
+	
+	# Draw grid lines, scaled to y-axis
+	abline(h=c(0:max.y-1),lty="solid",col="lightgray")
+	
+	# Redraw bars on top of grid lines
+	barplot(Degree.Distribution.Transpose,
+		beside=TRUE,
+		col=c("black","gray"),
+		yaxt="n",
+		add=T)
+	
+	# Add legend on top of grid lines
+	legend("topright",
+		legend=c("In-Degree","Out-Degree"),
+		fill=c("black","gray"),
+		border="black",
+		bg="white")
+	
+	# Saves plotted chart as .jpeg file
+	dev.off()
 }
