3f87a3df1970b30d1bc333535987fc41f1aa70bc
alicekfinton <50882812+alicekfinton@users.noreply.github.com>
2020-04-28 21:03:33 -0700

    Add MATLAB code

---
+++ data/Spring2020/Microarray Data Clustering/HeatMap_and_Clustering.m
@@ -0,0 +1,141 @@
+myData=xlsread('LK-Heat-Map_MATLAB-practice.xlsx')
+HeatMap(myData)
+HeatMap(myData, 'ColorMap','winter')-
+myData=xlsread('LK-Heat-Map_MATLAB-practice.xlsx')
+myData=xlsread('LKHeatMapPractice.xlsx')
+heatmap(myData)
+HeatMap(myData)
+B=MyData.';
+B='myData'.';
+B=transpose(myData)
+kmeans(myData)
+load myData;
+load myData
+plot myData
+clc
+HeatMap(myData)
+HeatMap(B)
+load('LKHeatMapPractice.xlsx')
+load(B)
+load('B')
+myData=xlsread('LKHeatMapPractice.xlsx')
+size(X)
+[idx3,C,sumdist3]=kmeans(X,4,'Weight');
+load myData
+plot(myData)
+load(myData)
+load('myData')
+xlsread('LKHeatMapPractice.xlsx')
+X=meas(:);
+X=measure(:)
+load B
+load('LK-Heat-Map_MATLAB-practice.xlsx');
+kmeans('myData',4);
+kmeans(myData,4)
+kmeans(B,4)
+kmeans(B,5)
+%-- 11/6/2019 1:33 PM --%
+myData=xlsread('LKHeatMapPractice.xlsx');
+kmeans(myData, 4)
+kmeans(myData, 10)
+A=xlsread('AFHeatMapClustering.xlsx');
+B=A.'
+kmeans=(B,10)
+kmeans(B,10)
+kmeans(A,10)
+kmeans(A,5)
+myVar=xlsread('AFHeatMapClustering.xlsx');
+kmeans(myVar,10)
+C=myData.';
+kmeans(C,4)
+kmeans(myData,10)
+kmeans(myData,5)
+kmeans(C,3)
+kmeans(myData,5)
+%-- 11/19/2019 9:14 AM --%
+myData=xlsread('Microarray-data_clustering.xlsx');
+B=myData.';
+kmeans(B,6)
+kmeans(myData,6)
+%-- 11/19/2019 10:11 AM --%
+myData=xlsread('Microarray-data-clustering.xlsx');
+myData=xlsread('Microarray-data_clustering.xlsx');
+B=myData.';\
+B=myData.';
+kmeans(B,10)
+HeatMap(myData)
+kmeans(B,10)
+kmeans(B,5)
+kmeans(B,10)
+kmeans(myData,10)
+clc
+kmeans(B,10)
+kmeans(B,3)
+clc
+kmeans(B,7)
+myVar=xlsread('AFHeatMapClustering.xlsx');
+kmeans(myVar,6)
+kmeans(B,10)
+kmeans(myData,20)
+clc
+kmeans(B,10)
+kmeans(B(:),10)
+clc
+kmeans(B(:),10);
+kmeans(myData(:),10);
+myData=xlsread('Microarray-data_clustering_wt-only.xlsx');
+B=myData.';
+kmeans(B,10)
+kmeans(B(:),10)
+clc
+cluster=kmeans(myData,10)
+cluster2=kmeans(B,6);
+cluster2=kmeans(B,4);
+%-- 11/19/2019 1:15 PM --%
+myData=xlsread('Microarray-data_clustering');
+B=myData.';
+kmeans(B,6)
+kmeans(myData,6)
+%-- 12/4/2019 2:09 PM --%
+myData=xlsread('Microarray-clustering_averages_replace-with-0.xlsx');
+B=myData.';
+kmeans(B,10)
+myData=xlsread('Microarray-clustering_averages_replace-with-0.xlsx');
+B=myData.';
+kmeans(B,10)
+A=xlsread('Microarray-clustering_averages_replace-with-0_fixed.xlsx');
+C=A.';
+kmeans(C,10)
+kmeans(A,10)
+D=kmeans(A,10);
+xlswrite('10-clusters_microarray_average_replace-with-0.xlsx',D);
+%-- 1/17/2020 10:35 AM --%
+A=xlsread('Microarray-data_clustering.xlsx');
+B=A.';
+[centers,U]=fcm(B,10);
+myData=xlsread('microarray-data_10-cluster_x-and-y.xlsx');
+heatmap(myData)
+HeatMap(myData)
+C=xlsread('Microarray-data_clustering.xlsx');
+HeatMap(C)
+D=xlsread('microarray-data_timepoint-averages.xlsx');
+HeatMap(D)
+%-- 1/29/2020 9:41 AM --%
+A=xlsread('avg_Microarray_heatmap_10-clusters-x.xlsx');
+HeatMap(A)
+HeatMap(A, 'Colormap', 'REDBLUE')
+map=[1 0 0
+1 1 1
+0 0 1};
+map=[1 0 0
+1 1 1
+0 0 1];
+HeatMap(A, 'Colormap', map)
+HeatMap(A, 'Colormap',redbluecmap)
+g = [g; flipud(g)];
+B=xlsread('avg_Microarray_10-clusters-x-y.xlsx');
+HeatMap(B)
+C=xlsread('delete-nonsignif_Microarray-avg_clusters-10-x-y.xlsx');
+HeatMap(C)
+HeatMap(C, 'Colormap',redbluecmap)
+clc
