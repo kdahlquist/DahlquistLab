93012c02941bf595f3d94a6bc0206b6114dd7983
nchun2 <122498296+nchun2@users.noreply.github.com>
2025-11-25 13:31:06 -0800

    corrected r script

--- data/Fall2024/Network Motif Comparison - 3 gene, 4 node networks/Analysis/Box Plot Making/R_script_boxplot.R
+++ data/Fall2024/Network Motif Comparison - 3 gene, 4 node networks/Analysis/Box Plot Making/R_script_boxplot.R
@@ -1,10 +1,13 @@
-noc <- log10(data$'No Constant')
-P <- log10(data$`P Constant`)
-B <- log10(data$`b Constant`)
-PB <- log10(data$`Pb Constant`)
+noc <- log10(R_script_input_trial_10_perm_overall_summary_sheet$'No Constant')
+P <- log10(R_script_input_trial_10_perm_overall_summary_sheet$'P Constant')
+B <- log10(R_script_input_trial_10_perm_overall_summary_sheet$'b Constant')
+PB <- log10(R_script_input_trial_10_perm_overall_summary_sheet$'Pb Constant')
 
+par(mar = c(6, 8, 6, 2))
 boxplot(PB, P, B, noc, 
-        main = "Log10 distribution of LSE when holding different parameters constant",
-        names = c("P and B", "P", "B", "No"),
+        main = "Distribution of LSE of Trial 10 Permutations when holding different parameters constant",
+        names = c("P and B constant", "P constant", "B constant", "No constant"),
         las = 2,
-        horizontal = TRUE)
+        horizontal = TRUE,
+        cex.names = 0.9,
+        ylim = c(-12, 2)) # Set x-axis range from -12 to 2
